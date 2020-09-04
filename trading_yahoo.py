232e3205d7161b4bf819561bfad0b50ea9ddb831
Francisco-Zafra <franciscoz1999@gmail.com>
2020-09-04 23:31:26 +0200

    some cleanup

--- trading_yahoo.py
+++ trading_yahoo.py
@@ -22,15 +22,11 @@ def stockbroker(stock, budget, wait):
         logging.info(stock + ' ' + str(data['Close'][len-1]))
         print(stock, str(data['Close'][len-1]))
         media_7_dias = sum(data['High'])/(data['High'].size)
-        if data['Close'][len-2] <= data['Close'][len-3] and data['Close'][len-3] <= data['Close'][len-4]:
-            cierre_anterior = data['Close'][len-2]
-            low_anterior = data['Low'][len-2]
-            high_anterior = data['High'][len-2]
+        #Condicion: Lleva tres veces seguidas bajando
+        if esta_bajando(data):
 
             cierre_actual = data['Close'][len-1]
-
-            ATR = max(high_anterior - low_anterior, high_anterior - cierre_anterior, cierre_anterior - low_anterior)
-            if(cierre_anterior - ATR > cierre_actual and cierre_actual < cartera):
+            if(cond_compra(data, cartera)):
                 comprar(cierre_actual, cartera, stock)
                 cartera -= cierre_actual * int(cartera/cierre_actual)
                 print('Me queda: ', cartera)
@@ -38,12 +34,42 @@ def stockbroker(stock, budget, wait):
                 
         for stock_vender in noVendidos():
             #print('Vender', stock_vender['coste'], 'a', data['Close'][len-1])
-            if stock_vender['coste']  < data['Close'][len-1] and stock_vender['empresa'] == stock:
+            if cond_venta(stock_vender, data, stock):
                 vender(data['Close'][len-1], stock_vender)
                 cartera = budget
                 print('Cartera vuelve a: ', budget)
                 logging.info('Cartera vuelve a: ' + str(budget))
         time.sleep(60 - wait)
+
+def esta_bajando(data):
+    vez1 = data['Close'][len-2]
+    vez2 = data['Close'][len-3]
+    cont = 4
+    while(vez1 == vez2):
+        vez2 = data['Close'][len-cont]
+        cont += 1
+        if(cont > len):
+            return False
+    vez3 = data['Close'][len-cont]
+    while(vez2 == vez3):
+        vez3 = data['Close'][len-cont]
+        cont += 1 
+        if(cont > len):
+            return False         
+
+    return vez1 < vez2 and vez2 < vez3
+
+def cond_compra(data, cartera):
+    len = data['High'].size
+    cierre_anterior = data['Close'][len-2]
+    low_anterior = data['Low'][len-2]
+    high_anterior = data['High'][len-2]
+    cierre_actual = data['Close'][len-1]
+
+    ATR = max(high_anterior - low_anterior, high_anterior - cierre_anterior, cierre_anterior - low_anterior)
+
+    return cierre_anterior - ATR > cierre_actual and cierre_actual < cartera
+    
 
 def comprar(valor, budget, empresa):
     logging.basicConfig(filename='example.log',level=logging.DEBUG)
@@ -68,6 +94,10 @@ def noVendidos():
         d = json.load(outfile)
         return d['compras']
 
+def cond_venta(stock_vender, data, stock):
+    return stock_vender['coste'] < data['Close'][len-1] and stock_vender['empresa'] == stock
+
+
 def vender(precio, stock):
     logging.basicConfig(filename='example.log',level=logging.DEBUG)
     print('Vende', stock['empresa'], precio)
@@ -86,9 +116,10 @@ def vender(precio, stock):
 
 def hebras(stocks):
     i = 0
+    budget = 100
     for s in stocks:
         hilo = threading.Thread(target=stockbroker, 
-                                args=(s, 100, i))
+                                args=(s, budget, i))
         hilo.start()
         i += 1
         print('Inicio Hebra: ', s)
